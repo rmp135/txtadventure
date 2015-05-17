6ef379550f98234270e1acb22f8671d84e33ee85
Ryan Poole <rmp135@gmail.com>
2015-05-17 19:51:52 +0000

    Actually fixed displaying the main page. For real this time.

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', function(req, res) {
+app.get('/', function(req, res) {
   res.render('index');
 });
 app.use('/api', apiroutes)
